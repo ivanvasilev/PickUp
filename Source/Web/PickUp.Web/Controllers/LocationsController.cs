cbed3d331ed66e030b3aa02735164dc38834c5dd
ivanvasilev <ivanvasilev_90@yahoo.com>
2016-02-22 11:47:44 +0200

    Added LocationsController

---
+++ Source/Web/PickUp.Web/Controllers/LocationsController.cs
@@ -0,0 +1,29 @@
+﻿using PickUp.Services.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PickUp.Web.Controllers
+{
+    public class LocationsController : BaseController
+    {
+        private ILocationsService locations;
+
+        public LocationsController(ILocationsService locations)
+        {
+            this.locations = locations;
+        }
+
+        // GET: Locations
+        public ActionResult GetAllLocations()
+        {
+            var allLocations = this.locations
+                .GetAll()
+                .ToList();
+
+            return this.View(allLocations);
+        }
+    }
+}
